53e6d10c8c43bd51e0f9119f96872d394ebe1d72
Karol <KarolGotkowski@gmx.de>
2020-06-01 10:17:09 +0200

    v0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gcam",
-    version="0.0.8",
+    version="0.0.9",
     author="Karol Gotkowski",
     author_email="KarolGotkowski@gmx.de",
     description="An easy to use framework that makes model predictions more interpretable for humans.",
